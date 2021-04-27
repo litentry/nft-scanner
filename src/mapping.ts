0d030feb06170bc536dca8fcffde4a9c26523453
zongxiong.chen <czxczf@gmail.com>
2021-04-27 14:08:12 +0200

    Add cryptopunk

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,4 +1,4 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { log, BigInt } from "@graphprotocol/graph-ts"
 import {
   WrappedMoonCatsRescue,
   Approval,
@@ -8,12 +8,18 @@ import {
 import { TokenHolder } from "../generated/schema"
 
 export function handleTransfer(event: Transfer): void {
-  const id = event.address + '#' + event.params.tokenId;
+  let id = event.address.toHex() + '#' + event.params.tokenId.toHex();
+  // let id = `${event.address}#${event.params.tokenId}`;
+  // let id = event.params.to.toHex() + event.params.tokenId.toHex();
+
+  log.info('handle transfer: id: {}, address: {}, tokenId: {}', [id, event.address.toString(), event.params.tokenId.toString()]);
+
   let entity = TokenHolder.load(id)
   if (entity == null) {
-    entity = new TokenHolder(id)
+    entity = new TokenHolder(id);
   }
   entity.contract = event.address;
   entity.tokenId = event.params.tokenId;
   entity.owner = event.params.to;
+  entity.save();
 }
