d27afcd34112d8a08136df895471973ff02e3478
zongxiong.chen <czxczf@gmail.com>
2021-05-13 02:05:43 +0200

    Load API from etherscan

--- generate.js
+++ generate.js
@@ -2,11 +2,24 @@ const _ = require('lodash');
 const fs = require('fs');
 const util = require('util');
 const yaml = require('js-yaml');
+var axios = require('axios');
 
+/// Load ABI from etherscan
+async function loadABI(path, contractAddress) {
+  contractAddress = '0x57f1887a8bf19b14fc0df6fd9b2acc9af147ea85';
+  const config = {
+    method: 'get',
+    url: `https://api.etherscan.io/api?module=contract&action=getabi&address=${contractAddress}`,
+  };
 
-_.templateSettings.interpolate = /{{([\s\S]+?)}}/g;
+  const resp = await axios(config);
+  const { result } = resp.data;
+  fs.writeFileSync(path, result);
+}
 
 /// source code configuration
+_.templateSettings.interpolate = /{{([\s\S]+?)}}/g;
+
 let compiled = null;
 function getCompiledSource(path) {
   if (_.isEmpty(compiled)) {
@@ -21,13 +34,12 @@ function generateSourcePair(path, contractName) {
   const generatedSource = compiled({
     contract_name: contractName,
   });
-  return [`./src/${contractName}Mapping.ts`, generatedSource];
+  return generatedSource;
 }
 
 function writeSource(path, content) {
   fs.writeFileSync(path, content);
 }
-
 
 /// subgraph configuration
 function loadSubgraphDataSourceTemplate(path) {
@@ -36,57 +48,64 @@ function loadSubgraphDataSourceTemplate(path) {
   return dataSourceTemplate;
 }
 
-function assignDataSouroce(dataSourceTemplate, contractMeta) {
+function assignDataSouroce(dataSourceTemplate, contract) {
   const cloned = _.cloneDeep(dataSourceTemplate);
-  cloned['name'] = contractMeta['contract_name'];
-  cloned['network'] = contractMeta['network'];
-  cloned['source']['address'] = contractMeta['contract_address'];
-  cloned['source']['abi'] = contractMeta['abi_name'];
-  cloned['source']['startBlock'] = contractMeta['start_block'];
-  cloned['mapping']['abis'][0]['name'] = contractMeta['abi_name'];
-  cloned['mapping']['abis'][0]['file'] = contractMeta['abi_file_path'];
-  cloned['mapping']['file'] = contractMeta['source_file_path'];
+  cloned['name'] = contract['name'];
+  cloned['network'] = contract['network'];
+  cloned['source']['address'] = contract['address'];
+  cloned['source']['abi'] = contract['abi_name'];
+  cloned['source']['startBlock'] = contract['start_block'];
+  cloned['mapping']['abis'][0]['name'] = contract['abi_name'];
+  cloned['mapping']['abis'][0]['file'] = contract['abi_file_path'];
+  cloned['mapping']['file'] = contract['source_file_path'];
   return cloned;
 }
 
 function writeSubgraph(path, dataSources) {
-  if (! _.isArray(dataSources)) {
+  if (!_.isArray(dataSources)) {
     dataSources = [dataSources];
   }
   const doc = {
     specVersion: '0.0.2',
-    schema: {file: './schema.graphql'},
-    dataSources: dataSources
+    schema: { file: './schema.graphql' },
+    dataSources: dataSources,
   };
 
-  console.log(util.inspect(doc, {showHidden: false, depth: null}));
-  fs.writeFileSync(path, yaml.dump(doc, {noRefs: true, quotingType: "\""}));
+  console.log(util.inspect(doc, { showHidden: false, depth: null }));
+  fs.writeFileSync(path, yaml.dump(doc, { noRefs: true, quotingType: '"' }));
 }
 
 
-try {
-  /// Generate mapping source codes
-  const dataSourceTemplatePath = './templates/dataSource.template.yaml';
-  const sourceTemplatePath = './templates/sourceMapping.template.ts';
-  const contractName = 'WrappedMoonCatsRescue';
+(async () => {
+  try {
+    const contract = {
+      name: 'WrappedMoonCatsRescue',
+      address: '0x7c40c393dc0f283f318791d746d894ddd3693572',
+      network: 'mainnet',
+      start_block: 12025810,
+    };
 
-  const [dstPath, source] = generateSourcePair(sourceTemplatePath, contractName);
-  writeSource(dstPath, source);
+    contract['abi_name'] = contract.name;
+    contract['abi_file_path'] = `./abis/${contract.name}.json`;
+    contract['source_file_path'] = `./src/${contract.name}Mapping.ts`;
 
-  /// Generate subgraph
-  const dataSourceTemplate = loadSubgraphDataSourceTemplate(dataSourceTemplatePath);
-  const contractMeta = {
-    contract_name: contractName,
-    network: 'mainnet',
-    contract_address: '0x7c40c393dc0f283f318791d746d894ddd3693572',
-    abi_name: contractName,
-    start_block: 12025810,
-    abi_file_path: `./abis/${contractName}.json`,
-    source_file_path: dstPath,
-  };
-  const dataSource = assignDataSouroce(dataSourceTemplate, contractMeta);
-  const subgraphPath = './subgraph.yaml';
-  writeSubgraph(subgraphPath, [dataSource]);
-} catch (e) {
-  console.log(e);
-}
+    await loadABI(contract.abi_file_path, contract.address);
+
+    /// Generate mapping source codes
+    const dataSourceTemplatePath = './templates/dataSource.template.yaml';
+    const sourceTemplatePath = './templates/sourceMapping.template.ts';
+
+    const source = generateSourcePair(sourceTemplatePath, contract.name);
+    writeSource(contract.source_file_path, source);
+
+    /// Generate subgraph
+    const subgraphPath = './subgraph.yaml';
+    const dataSourceTemplate = loadSubgraphDataSourceTemplate(dataSourceTemplatePath);
+    const dataSource = assignDataSouroce(dataSourceTemplate, contract);
+
+    writeSubgraph(subgraphPath, [dataSource]);
+    console.log(`Generate [${contract.name}] abi: ${contract.abi_file_path}, source: ${contract.source_file_path}.`);
+  } catch (e) {
+    console.log(e);
+  }
+})();
--- generated/WrappedMoonCatsRescue/WrappedMoonCatsRescue.ts
+++ generated/WrappedMoonCatsRescue/WrappedMoonCatsRescue.ts
@@ -62,6 +62,138 @@ export class ApprovalForAll__Params {
   }
 }
 
+export class ControllerAdded extends ethereum.Event {
+  get params(): ControllerAdded__Params {
+    return new ControllerAdded__Params(this);
+  }
+}
+
+export class ControllerAdded__Params {
+  _event: ControllerAdded;
+
+  constructor(event: ControllerAdded) {
+    this._event = event;
+  }
+
+  get controller(): Address {
+    return this._event.parameters[0].value.toAddress();
+  }
+}
+
+export class ControllerRemoved extends ethereum.Event {
+  get params(): ControllerRemoved__Params {
+    return new ControllerRemoved__Params(this);
+  }
+}
+
+export class ControllerRemoved__Params {
+  _event: ControllerRemoved;
+
+  constructor(event: ControllerRemoved) {
+    this._event = event;
+  }
+
+  get controller(): Address {
+    return this._event.parameters[0].value.toAddress();
+  }
+}
+
+export class NameMigrated extends ethereum.Event {
+  get params(): NameMigrated__Params {
+    return new NameMigrated__Params(this);
+  }
+}
+
+export class NameMigrated__Params {
+  _event: NameMigrated;
+
+  constructor(event: NameMigrated) {
+    this._event = event;
+  }
+
+  get id(): BigInt {
+    return this._event.parameters[0].value.toBigInt();
+  }
+
+  get owner(): Address {
+    return this._event.parameters[1].value.toAddress();
+  }
+
+  get expires(): BigInt {
+    return this._event.parameters[2].value.toBigInt();
+  }
+}
+
+export class NameRegistered extends ethereum.Event {
+  get params(): NameRegistered__Params {
+    return new NameRegistered__Params(this);
+  }
+}
+
+export class NameRegistered__Params {
+  _event: NameRegistered;
+
+  constructor(event: NameRegistered) {
+    this._event = event;
+  }
+
+  get id(): BigInt {
+    return this._event.parameters[0].value.toBigInt();
+  }
+
+  get owner(): Address {
+    return this._event.parameters[1].value.toAddress();
+  }
+
+  get expires(): BigInt {
+    return this._event.parameters[2].value.toBigInt();
+  }
+}
+
+export class NameRenewed extends ethereum.Event {
+  get params(): NameRenewed__Params {
+    return new NameRenewed__Params(this);
+  }
+}
+
+export class NameRenewed__Params {
+  _event: NameRenewed;
+
+  constructor(event: NameRenewed) {
+    this._event = event;
+  }
+
+  get id(): BigInt {
+    return this._event.parameters[0].value.toBigInt();
+  }
+
+  get expires(): BigInt {
+    return this._event.parameters[1].value.toBigInt();
+  }
+}
+
+export class OwnershipTransferred extends ethereum.Event {
+  get params(): OwnershipTransferred__Params {
+    return new OwnershipTransferred__Params(this);
+  }
+}
+
+export class OwnershipTransferred__Params {
+  _event: OwnershipTransferred;
+
+  constructor(event: OwnershipTransferred) {
+    this._event = event;
+  }
+
+  get previousOwner(): Address {
+    return this._event.parameters[0].value.toAddress();
+  }
+
+  get newOwner(): Address {
+    return this._event.parameters[1].value.toAddress();
+  }
+}
+
 export class Transfer extends ethereum.Event {
   get params(): Transfer__Params {
     return new Transfer__Params(this);
@@ -93,6 +225,40 @@ export class WrappedMoonCatsRescue extends ethereum.SmartContract {
     return new WrappedMoonCatsRescue("WrappedMoonCatsRescue", address);
   }
 
+  GRACE_PERIOD(): BigInt {
+    let result = super.call("GRACE_PERIOD", "GRACE_PERIOD():(uint256)", []);
+
+    return result[0].toBigInt();
+  }
+
+  try_GRACE_PERIOD(): ethereum.CallResult<BigInt> {
+    let result = super.tryCall("GRACE_PERIOD", "GRACE_PERIOD():(uint256)", []);
+    if (result.reverted) {
+      return new ethereum.CallResult();
+    }
+    let value = result.value;
+    return ethereum.CallResult.fromValue(value[0].toBigInt());
+  }
+
+  available(id: BigInt): boolean {
+    let result = super.call("available", "available(uint256):(bool)", [
+      ethereum.Value.fromUnsignedBigInt(id)
+    ]);
+
+    return result[0].toBoolean();
+  }
+
+  try_available(id: BigInt): ethereum.CallResult<boolean> {
+    let result = super.tryCall("available", "available(uint256):(bool)", [
+      ethereum.Value.fromUnsignedBigInt(id)
+    ]);
+    if (result.reverted) {
+      return new ethereum.CallResult();
+    }
+    let value = result.value;
+    return ethereum.CallResult.fromValue(value[0].toBoolean());
+  }
+
   balanceOf(owner: Address): BigInt {
     let result = super.call("balanceOf", "balanceOf(address):(uint256)", [
       ethereum.Value.fromAddress(owner)
@@ -110,6 +276,55 @@ export class WrappedMoonCatsRescue extends ethereum.SmartContract {
     }
     let value = result.value;
     return ethereum.CallResult.fromValue(value[0].toBigInt());
+  }
+
+  baseNode(): Bytes {
+    let result = super.call("baseNode", "baseNode():(bytes32)", []);
+
+    return result[0].toBytes();
+  }
+
+  try_baseNode(): ethereum.CallResult<Bytes> {
+    let result = super.tryCall("baseNode", "baseNode():(bytes32)", []);
+    if (result.reverted) {
+      return new ethereum.CallResult();
+    }
+    let value = result.value;
+    return ethereum.CallResult.fromValue(value[0].toBytes());
+  }
+
+  controllers(param0: Address): boolean {
+    let result = super.call("controllers", "controllers(address):(bool)", [
+      ethereum.Value.fromAddress(param0)
+    ]);
+
+    return result[0].toBoolean();
+  }
+
+  try_controllers(param0: Address): ethereum.CallResult<boolean> {
+    let result = super.tryCall("controllers", "controllers(address):(bool)", [
+      ethereum.Value.fromAddress(param0)
+    ]);
+    if (result.reverted) {
+      return new ethereum.CallResult();
+    }
+    let value = result.value;
+    return ethereum.CallResult.fromValue(value[0].toBoolean());
+  }
+
+  ens(): Address {
+    let result = super.call("ens", "ens():(address)", []);
+
+    return result[0].toAddress();
+  }
+
+  try_ens(): ethereum.CallResult<Address> {
+    let result = super.tryCall("ens", "ens():(address)", []);
+    if (result.reverted) {
+      return new ethereum.CallResult();
+    }
+    let value = result.value;
+    return ethereum.CallResult.fromValue(value[0].toAddress());
   }
 
   getApproved(tokenId: BigInt): Address {
@@ -159,6 +374,57 @@ export class WrappedMoonCatsRescue extends ethereum.SmartContract {
     return ethereum.CallResult.fromValue(value[0].toBoolean());
   }
 
+  isOwner(): boolean {
+    let result = super.call("isOwner", "isOwner():(bool)", []);
+
+    return result[0].toBoolean();
+  }
+
+  try_isOwner(): ethereum.CallResult<boolean> {
+    let result = super.tryCall("isOwner", "isOwner():(bool)", []);
+    if (result.reverted) {
+      return new ethereum.CallResult();
+    }
+    let value = result.value;
+    return ethereum.CallResult.fromValue(value[0].toBoolean());
+  }
+
+  nameExpires(id: BigInt): BigInt {
+    let result = super.call("nameExpires", "nameExpires(uint256):(uint256)", [
+      ethereum.Value.fromUnsignedBigInt(id)
+    ]);
+
+    return result[0].toBigInt();
+  }
+
+  try_nameExpires(id: BigInt): ethereum.CallResult<BigInt> {
+    let result = super.tryCall(
+      "nameExpires",
+      "nameExpires(uint256):(uint256)",
+      [ethereum.Value.fromUnsignedBigInt(id)]
+    );
+    if (result.reverted) {
+      return new ethereum.CallResult();
+    }
+    let value = result.value;
+    return ethereum.CallResult.fromValue(value[0].toBigInt());
+  }
+
+  owner(): Address {
+    let result = super.call("owner", "owner():(address)", []);
+
+    return result[0].toAddress();
+  }
+
+  try_owner(): ethereum.CallResult<Address> {
+    let result = super.tryCall("owner", "owner():(address)", []);
+    if (result.reverted) {
+      return new ethereum.CallResult();
+    }
+    let value = result.value;
+    return ethereum.CallResult.fromValue(value[0].toAddress());
+  }
+
   ownerOf(tokenId: BigInt): Address {
     let result = super.call("ownerOf", "ownerOf(uint256):(address)", [
       ethereum.Value.fromUnsignedBigInt(tokenId)
@@ -178,27 +444,182 @@ export class WrappedMoonCatsRescue extends ethereum.SmartContract {
     return ethereum.CallResult.fromValue(value[0].toAddress());
   }
 
-  supportsInterface(interfaceId: Bytes): boolean {
+  register(id: BigInt, owner: Address, duration: BigInt): BigInt {
+    let result = super.call(
+      "register",
+      "register(uint256,address,uint256):(uint256)",
+      [
+        ethereum.Value.fromUnsignedBigInt(id),
+        ethereum.Value.fromAddress(owner),
+        ethereum.Value.fromUnsignedBigInt(duration)
+      ]
+    );
+
+    return result[0].toBigInt();
+  }
+
+  try_register(
+    id: BigInt,
+    owner: Address,
+    duration: BigInt
+  ): ethereum.CallResult<BigInt> {
+    let result = super.tryCall(
+      "register",
+      "register(uint256,address,uint256):(uint256)",
+      [
+        ethereum.Value.fromUnsignedBigInt(id),
+        ethereum.Value.fromAddress(owner),
+        ethereum.Value.fromUnsignedBigInt(duration)
+      ]
+    );
+    if (result.reverted) {
+      return new ethereum.CallResult();
+    }
+    let value = result.value;
+    return ethereum.CallResult.fromValue(value[0].toBigInt());
+  }
+
+  registerOnly(id: BigInt, owner: Address, duration: BigInt): BigInt {
+    let result = super.call(
+      "registerOnly",
+      "registerOnly(uint256,address,uint256):(uint256)",
+      [
+        ethereum.Value.fromUnsignedBigInt(id),
+        ethereum.Value.fromAddress(owner),
+        ethereum.Value.fromUnsignedBigInt(duration)
+      ]
+    );
+
+    return result[0].toBigInt();
+  }
+
+  try_registerOnly(
+    id: BigInt,
+    owner: Address,
+    duration: BigInt
+  ): ethereum.CallResult<BigInt> {
+    let result = super.tryCall(
+      "registerOnly",
+      "registerOnly(uint256,address,uint256):(uint256)",
+      [
+        ethereum.Value.fromUnsignedBigInt(id),
+        ethereum.Value.fromAddress(owner),
+        ethereum.Value.fromUnsignedBigInt(duration)
+      ]
+    );
+    if (result.reverted) {
+      return new ethereum.CallResult();
+    }
+    let value = result.value;
+    return ethereum.CallResult.fromValue(value[0].toBigInt());
+  }
+
+  renew(id: BigInt, duration: BigInt): BigInt {
+    let result = super.call("renew", "renew(uint256,uint256):(uint256)", [
+      ethereum.Value.fromUnsignedBigInt(id),
+      ethereum.Value.fromUnsignedBigInt(duration)
+    ]);
+
+    return result[0].toBigInt();
+  }
+
+  try_renew(id: BigInt, duration: BigInt): ethereum.CallResult<BigInt> {
+    let result = super.tryCall("renew", "renew(uint256,uint256):(uint256)", [
+      ethereum.Value.fromUnsignedBigInt(id),
+      ethereum.Value.fromUnsignedBigInt(duration)
+    ]);
+    if (result.reverted) {
+      return new ethereum.CallResult();
+    }
+    let value = result.value;
+    return ethereum.CallResult.fromValue(value[0].toBigInt());
+  }
+
+  supportsInterface(interfaceID: Bytes): boolean {
     let result = super.call(
       "supportsInterface",
       "supportsInterface(bytes4):(bool)",
-      [ethereum.Value.fromFixedBytes(interfaceId)]
+      [ethereum.Value.fromFixedBytes(interfaceID)]
     );
 
     return result[0].toBoolean();
   }
 
-  try_supportsInterface(interfaceId: Bytes): ethereum.CallResult<boolean> {
+  try_supportsInterface(interfaceID: Bytes): ethereum.CallResult<boolean> {
     let result = super.tryCall(
       "supportsInterface",
       "supportsInterface(bytes4):(bool)",
-      [ethereum.Value.fromFixedBytes(interfaceId)]
+      [ethereum.Value.fromFixedBytes(interfaceID)]
     );
     if (result.reverted) {
       return new ethereum.CallResult();
     }
     let value = result.value;
     return ethereum.CallResult.fromValue(value[0].toBoolean());
+  }
+}
+
+export class ConstructorCall extends ethereum.Call {
+  get inputs(): ConstructorCall__Inputs {
+    return new ConstructorCall__Inputs(this);
+  }
+
+  get outputs(): ConstructorCall__Outputs {
+    return new ConstructorCall__Outputs(this);
+  }
+}
+
+export class ConstructorCall__Inputs {
+  _call: ConstructorCall;
+
+  constructor(call: ConstructorCall) {
+    this._call = call;
+  }
+
+  get _ens(): Address {
+    return this._call.inputValues[0].value.toAddress();
+  }
+
+  get _baseNode(): Bytes {
+    return this._call.inputValues[1].value.toBytes();
+  }
+}
+
+export class ConstructorCall__Outputs {
+  _call: ConstructorCall;
+
+  constructor(call: ConstructorCall) {
+    this._call = call;
+  }
+}
+
+export class AddControllerCall extends ethereum.Call {
+  get inputs(): AddControllerCall__Inputs {
+    return new AddControllerCall__Inputs(this);
+  }
+
+  get outputs(): AddControllerCall__Outputs {
+    return new AddControllerCall__Outputs(this);
+  }
+}
+
+export class AddControllerCall__Inputs {
+  _call: AddControllerCall;
+
+  constructor(call: AddControllerCall) {
+    this._call = call;
+  }
+
+  get controller(): Address {
+    return this._call.inputValues[0].value.toAddress();
+  }
+}
+
+export class AddControllerCall__Outputs {
+  _call: AddControllerCall;
+
+  constructor(call: AddControllerCall) {
+    this._call = call;
   }
 }
 
@@ -232,6 +653,218 @@ export class ApproveCall__Outputs {
   _call: ApproveCall;
 
   constructor(call: ApproveCall) {
+    this._call = call;
+  }
+}
+
+export class ReclaimCall extends ethereum.Call {
+  get inputs(): ReclaimCall__Inputs {
+    return new ReclaimCall__Inputs(this);
+  }
+
+  get outputs(): ReclaimCall__Outputs {
+    return new ReclaimCall__Outputs(this);
+  }
+}
+
+export class ReclaimCall__Inputs {
+  _call: ReclaimCall;
+
+  constructor(call: ReclaimCall) {
+    this._call = call;
+  }
+
+  get id(): BigInt {
+    return this._call.inputValues[0].value.toBigInt();
+  }
+
+  get owner(): Address {
+    return this._call.inputValues[1].value.toAddress();
+  }
+}
+
+export class ReclaimCall__Outputs {
+  _call: ReclaimCall;
+
+  constructor(call: ReclaimCall) {
+    this._call = call;
+  }
+}
+
+export class RegisterCall extends ethereum.Call {
+  get inputs(): RegisterCall__Inputs {
+    return new RegisterCall__Inputs(this);
+  }
+
+  get outputs(): RegisterCall__Outputs {
+    return new RegisterCall__Outputs(this);
+  }
+}
+
+export class RegisterCall__Inputs {
+  _call: RegisterCall;
+
+  constructor(call: RegisterCall) {
+    this._call = call;
+  }
+
+  get id(): BigInt {
+    return this._call.inputValues[0].value.toBigInt();
+  }
+
+  get owner(): Address {
+    return this._call.inputValues[1].value.toAddress();
+  }
+
+  get duration(): BigInt {
+    return this._call.inputValues[2].value.toBigInt();
+  }
+}
+
+export class RegisterCall__Outputs {
+  _call: RegisterCall;
+
+  constructor(call: RegisterCall) {
+    this._call = call;
+  }
+
+  get value0(): BigInt {
+    return this._call.outputValues[0].value.toBigInt();
+  }
+}
+
+export class RegisterOnlyCall extends ethereum.Call {
+  get inputs(): RegisterOnlyCall__Inputs {
+    return new RegisterOnlyCall__Inputs(this);
+  }
+
+  get outputs(): RegisterOnlyCall__Outputs {
+    return new RegisterOnlyCall__Outputs(this);
+  }
+}
+
+export class RegisterOnlyCall__Inputs {
+  _call: RegisterOnlyCall;
+
+  constructor(call: RegisterOnlyCall) {
+    this._call = call;
+  }
+
+  get id(): BigInt {
+    return this._call.inputValues[0].value.toBigInt();
+  }
+
+  get owner(): Address {
+    return this._call.inputValues[1].value.toAddress();
+  }
+
+  get duration(): BigInt {
+    return this._call.inputValues[2].value.toBigInt();
+  }
+}
+
+export class RegisterOnlyCall__Outputs {
+  _call: RegisterOnlyCall;
+
+  constructor(call: RegisterOnlyCall) {
+    this._call = call;
+  }
+
+  get value0(): BigInt {
+    return this._call.outputValues[0].value.toBigInt();
+  }
+}
+
+export class RemoveControllerCall extends ethereum.Call {
+  get inputs(): RemoveControllerCall__Inputs {
+    return new RemoveControllerCall__Inputs(this);
+  }
+
+  get outputs(): RemoveControllerCall__Outputs {
+    return new RemoveControllerCall__Outputs(this);
+  }
+}
+
+export class RemoveControllerCall__Inputs {
+  _call: RemoveControllerCall;
+
+  constructor(call: RemoveControllerCall) {
+    this._call = call;
+  }
+
+  get controller(): Address {
+    return this._call.inputValues[0].value.toAddress();
+  }
+}
+
+export class RemoveControllerCall__Outputs {
+  _call: RemoveControllerCall;
+
+  constructor(call: RemoveControllerCall) {
+    this._call = call;
+  }
+}
+
+export class RenewCall extends ethereum.Call {
+  get inputs(): RenewCall__Inputs {
+    return new RenewCall__Inputs(this);
+  }
+
+  get outputs(): RenewCall__Outputs {
+    return new RenewCall__Outputs(this);
+  }
+}
+
+export class RenewCall__Inputs {
+  _call: RenewCall;
+
+  constructor(call: RenewCall) {
+    this._call = call;
+  }
+
+  get id(): BigInt {
+    return this._call.inputValues[0].value.toBigInt();
+  }
+
+  get duration(): BigInt {
+    return this._call.inputValues[1].value.toBigInt();
+  }
+}
+
+export class RenewCall__Outputs {
+  _call: RenewCall;
+
+  constructor(call: RenewCall) {
+    this._call = call;
+  }
+
+  get value0(): BigInt {
+    return this._call.outputValues[0].value.toBigInt();
+  }
+}
+
+export class RenounceOwnershipCall extends ethereum.Call {
+  get inputs(): RenounceOwnershipCall__Inputs {
+    return new RenounceOwnershipCall__Inputs(this);
+  }
+
+  get outputs(): RenounceOwnershipCall__Outputs {
+    return new RenounceOwnershipCall__Outputs(this);
+  }
+}
+
+export class RenounceOwnershipCall__Inputs {
+  _call: RenounceOwnershipCall;
+
+  constructor(call: RenounceOwnershipCall) {
+    this._call = call;
+  }
+}
+
+export class RenounceOwnershipCall__Outputs {
+  _call: RenounceOwnershipCall;
+
+  constructor(call: RenounceOwnershipCall) {
     this._call = call;
   }
 }
@@ -303,7 +936,7 @@ export class SafeTransferFrom1Call__Inputs {
     return this._call.inputValues[2].value.toBigInt();
   }
 
-  get data(): Bytes {
+  get _data(): Bytes {
     return this._call.inputValues[3].value.toBytes();
   }
 }
@@ -333,11 +966,11 @@ export class SetApprovalForAllCall__Inputs {
     this._call = call;
   }
 
-  get operator(): Address {
+  get to(): Address {
     return this._call.inputValues[0].value.toAddress();
   }
 
-  get _approved(): boolean {
+  get approved(): boolean {
     return this._call.inputValues[1].value.toBoolean();
   }
 }
@@ -346,6 +979,36 @@ export class SetApprovalForAllCall__Outputs {
   _call: SetApprovalForAllCall;
 
   constructor(call: SetApprovalForAllCall) {
+    this._call = call;
+  }
+}
+
+export class SetResolverCall extends ethereum.Call {
+  get inputs(): SetResolverCall__Inputs {
+    return new SetResolverCall__Inputs(this);
+  }
+
+  get outputs(): SetResolverCall__Outputs {
+    return new SetResolverCall__Outputs(this);
+  }
+}
+
+export class SetResolverCall__Inputs {
+  _call: SetResolverCall;
+
+  constructor(call: SetResolverCall) {
+    this._call = call;
+  }
+
+  get resolver(): Address {
+    return this._call.inputValues[0].value.toAddress();
+  }
+}
+
+export class SetResolverCall__Outputs {
+  _call: SetResolverCall;
+
+  constructor(call: SetResolverCall) {
     this._call = call;
   }
 }
@@ -384,6 +1047,36 @@ export class TransferFromCall__Outputs {
   _call: TransferFromCall;
 
   constructor(call: TransferFromCall) {
+    this._call = call;
+  }
+}
+
+export class TransferOwnershipCall extends ethereum.Call {
+  get inputs(): TransferOwnershipCall__Inputs {
+    return new TransferOwnershipCall__Inputs(this);
+  }
+
+  get outputs(): TransferOwnershipCall__Outputs {
+    return new TransferOwnershipCall__Outputs(this);
+  }
+}
+
+export class TransferOwnershipCall__Inputs {
+  _call: TransferOwnershipCall;
+
+  constructor(call: TransferOwnershipCall) {
+    this._call = call;
+  }
+
+  get newOwner(): Address {
+    return this._call.inputValues[0].value.toAddress();
+  }
+}
+
+export class TransferOwnershipCall__Outputs {
+  _call: TransferOwnershipCall;
+
+  constructor(call: TransferOwnershipCall) {
     this._call = call;
   }
 }
